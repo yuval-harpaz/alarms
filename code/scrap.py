ee75f7ac0839e551476866f2c90256af5af2ef18
yuval <yuval@lenovo.2020>
2023-05-10 09:55:04 +0300

    traj

--- code/scrap.py
+++ code/scrap.py
@@ -6,6 +6,9 @@ import matplotlib
 import pandas as pd
 import os
 from datetime import datetime
+
+# https://he.wikipedia.org/wiki/%D7%94%D7%9C%D7%97%D7%99%D7%9E%D7%94_%D7%91%D7%A8%D7%A6%D7%95%D7%A2%D7%AA_%D7%A2%D7%96%D7%94_%D7%9C%D7%90%D7%97%D7%A8_%D7%9E%D7%91%D7%A6%D7%A2_%D7%A9%D7%95%D7%9E%D7%A8_%D7%94%D7%97%D7%95%D7%9E%D7%95%D7%AA
+
 os.chdir('/home/innereye/alarms/data/')
 alarms = pd.read_csv('alarms.csv')
 strikes = pd.read_csv('strikes_in_gazza.csv')
