4be04c7e682609bde39b19644db9cd478cce626c
yuval <yuval@lenovo.2020>
2023-06-07 21:12:12 +0300

    Mag

--- code/earthquakes2.py
+++ code/earthquakes2.py
@@ -4,7 +4,8 @@ import folium
 import pandas as pd
 import numpy as np
 import os
-
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 update = pd.read_csv('https://eq.gsi.gov.il/en/earthquake/files/last30_event.csv')
 
 local = '/home/innereye/alarms/'
@@ -13,33 +14,55 @@ if os.path.isdir(local):
 df = pd.read_csv('data/earthquakes.csv')
 df = df.merge(update, how='outer')
 dt = pd.to_datetime(df['DateTime']).to_numpy()
-mag3 = np.asarray([df['Md'], df['Mb'], df['Mw']]).T
-mag = np.max(mag3, axis=1)
-imax = np.argmax(mag3, axis=1)
-mag[mag3[:, 2] > 0] = mag3[mag3[:, 2] > 0, 2]
-M = []  # take Mw when possible
+mag4 = np.asarray([df['Md'], df['Mb'], df['Mw'], df['Mag']]).T
+mag = np.zeros(len(mag4))
+M = np.zeros(len(mag4), str)
+mags = ['Md','Mb', 'Mw', 'Mag']
+mag4[np.isnan(mag4)] = 0
 for ii in range(len(dt)):
-    if mag3[ii, 2] == 0:
-        M.append(df.columns[2+imax[ii]])
+    if mag[ii, 3] > 0:  # prefer Mag, although it is the same as Mw
+        mag[ii] = mag4[ii, 3]
+        M[ii] = mags[3]
+    elif mag[ii, 2] > 0:  # Mw as second option
+        mag[ii] = mag4[ii, 2]
+        M[ii] = mags[2]
     else:
-        M.append('Mw')
+        imax = np.argmax(mag4[ii, :2])
+        mag[ii] = mag4[ii, imax]
+        M[ii] = mags[imax]
+# mag = np.max(mag4, axis=1)
+# imax = np.argmax(mag4, axis=1)
+# mag[mag4[:, 2] > 0] = mag4[mag4[:, 2] > 0, 2]
+# M = []  # take Mw when possible
+# for ii in range(len(dt)):
+#     if mag4[ii, 3] > 0:
+#         M.append('Mag')
+#     elif mag4[ii, 2] == 0:
+#         M.append(df.columns[2+imax[ii]])
+#     else:
+#         M.append('Mw')
 now = np.datetime64('now', 'ns')
+nowisr = pd.to_datetime(now, utc=True, unit='s').astimezone(tz='Israel')
+# dtc = dt[n].replace(tzinfo=None)
+# dt = np.asarray([pd.to_datetime(ds, utc=True, unit='s').astimezone(tz='Israel') for ds in df1['time']])
+nowstr = str(nowisr)[:16].replace('T', ' ')
 dif = now-dt
-dif = dif.astype('timedelta64[D]')
-lin = dif.copy().astype(int)
-group_index = np.ones(len(lin), int)*4
-four = np.zeros((len(lin), 4))
+dif_sec = dif.astype('timedelta64[s]').astype(float)
+dif_days = dif_sec/60**2/24
+# lin = dif.copy().astype(int)
+group_index = np.ones(len(dif_days), int)*4
+four = np.zeros((len(dif_days), 4))
 four[:, 2] = 1
 four[:, 3] = 0.2
 ccc = [365, 30, 7, 1]
 co = [[0, 1, 0.5, 1], [1, 0.75, 0.5, 1], [1, 0, 0, 1], [0, 0, 0, 1]]
 for ii, cc in enumerate(ccc):
     for ll in range(4):
-        four[lin <= ccc[ii], ll] = co[ii][ll]
-        group_index[lin <= ccc[ii]] = 3-ii
+        four[dif_days <= ccc[ii], ll] = co[ii][ll]
+        group_index[dif_days <= ccc[ii]] = 3-ii
 
-title_html = '''
-             <h3 align="center" style="font-size:16px"><b>Earthquakes measured in Israel since 2000, data from <a href="https://eq.gsi.gov.il/heb/earthquake/lastEarthquakes.php" target="_blank">THE GEOLOGICAL SURVEY OF ISRAEL</a></b></h3>
+title_html = f'''
+             <h3 align="center" style="font-size:16px"><b>Earthquakes measured in Israel since 2000, data from <a href="https://eq.gsi.gov.il/heb/earthquake/lastEarthquakes.php" target="_blank">THE GEOLOGICAL SURVEY OF ISRAEL</a>. last checked: {nowstr}</b></h3>
              '''
 
 lgd_txt = '<span style="color: {col};">{txt}</span>'
@@ -63,12 +86,12 @@ for ii in range(len(df)):
     lon = df['Long'][ii]
     # dt = df['DateTime'][ii]
     c = colors.to_hex(four[ii, :3])
-    deapth = ''
+    depth = ''
     d = df['Depth(Km)'][ii]
     if d > 0:
-        deapth = ', deapth: '+str(d)+'Km'
+        depth = ', depth: '+str(d)+'Km'
     tip = df['DateTime'][ii][:-4].replace('T', ' ')
-    tip = tip+'<br> '+M[ii]+': '+str(mag[ii])+deapth
+    tip = tip+'<br> '+M[ii]+': '+str(mag[ii])+depth
     folium.CircleMarker(location=[lat, lon],
                         tooltip=tip,
                         radius=mag[ii]**2/2,
@@ -79,7 +102,7 @@ for ii in range(len(df)):
                         fill_opacity=four[ii, 3]
                         ).add_to(grp[group_index[ii]])
 for ig in range(5):
-    grp[ig].add_to(map)
+    grp[4-ig].add_to(map)
 folium.map.LayerControl('topleft', collapsed=False).add_to(map)
-map.save("docs/tmp.html")
-# df.to_csv('data/earthquakes.csv', index=False)
+map.save("docs/earthquakes_by_time.html")
+df.to_csv('data/earthquakes.csv', index=False)
