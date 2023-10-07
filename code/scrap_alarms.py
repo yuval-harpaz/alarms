d892eb961314a073c1fe66d7f37a080467ddaec6
yuval <yuval@lenovo.2020>
2023-10-07 16:19:58 +0300

    10min update

--- code/scrap_alarms.py
+++ code/scrap_alarms.py
@@ -8,7 +8,9 @@ os.chdir(local)
 islocal = True
 df = pd.read_csv('data/alarms.csv')
 date = [x[:10] for x in df['time'].values]
-df = df[np.array(date) == '2023-10-07']
+first = np.where(np.array(date) == '2023-10-07')[0][0]
+# df = df[np.array(date) == '2023-10-07']
+df = df[first:]
 loc = np.unique(df['cities'])
 count = []
 for iloc in range(len(loc)):
@@ -17,4 +19,6 @@ df_count = pd.DataFrame(loc, columns=['loc'])
 df_count['count'] = count
 df_count = df_count.sort_values('count')
 n = len(df_count)
-for ii in range(n):
+for ii in range(n-50, n):
+    print(df_count.iloc[ii]['loc']+': '+str(df_count.iloc[ii]['count']))
+
