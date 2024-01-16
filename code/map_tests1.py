739e36d8b981f04349e466f3614edf2e4fe23788
yuval <yuval@lenovo.2020>
2024-01-16 22:27:08 +0200

    kidnapped

--- code/map_tests1.py
+++ code/map_tests1.py
@@ -12,10 +12,10 @@ if os.path.isdir(local):
     # sys.path.append(local + 'code')
 
 haa = pd.read_csv('data/deaths_haaretz+.csv')
-names = pd.read_excel('data/deaths_by_loc.xlsx', 'export')
+names = pd.read_csv('data/oct_7_9.csv')
 # names_row = np.array(names.index)+2
 missing = []
-for ii in range(2, len(names)+3):
+for ii in range(2, len(names)+2):
     if ii not in haa['map_row'].values:
       missing.append(ii)
 
@@ -28,6 +28,21 @@ for x in np.unique(row):
 maprow = 75
 haarow = np.where(haa['map_row'] == maprow)[0][0]
 print(haa['name'][haarow]+' '+names['fullName'][maprow-2])
+missmatch = []
+for ii in range(2, len(names)+2):
+    haarow = np.where(haa['map_row'] == ii)[0][0]
+    namem = names['fullName'][ii-2]
+    rankm = str(names['rank'][ii-2]).replace("(מיל')",'').replace("(מיל׳)",'').replace('"', '״').strip()
+    rankh = str(haa['rank'][haarow]).replace("(מיל')",'').replace("(מיל׳)",'').replace('"', '״').strip()
+    jdf = haa['idf_row'][haarow]
+    nans = np.sum(np.array([rankm, rankh]) == 'nan')
+    if nans == 1:
+        print(f'{namem} map: {rankm} haaretz: {rankh}')
+        missmatch.append(f'{namem} map: {rankm} haaretz: {rankh}')
+    elif nans ==0 and rankm != rankh:
+        print(f'{namem} map: {rankm} haaretz: {rankh}')
+        missmatch.append(f'{namem} map: {rankm} haaretz: {rankh}')
+
 #
 # coo = pd.read_excel('data/deaths_by_loc.xlsx', 'coo')
 # names = pd.read_excel('data/deaths_by_loc.xlsx', 'export')
