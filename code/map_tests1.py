36074962808bb62ee30b944411e4280cb53389ca
yuval <yuval@lenovo.2020>
2024-01-17 22:25:36 +0200

    fix also for haa

--- code/map_tests1.py
+++ code/map_tests1.py
@@ -72,31 +72,16 @@ for ii in range(2, len(names) + 2):
         if np.abs(ageh - agem) > 1:
             missmatch.append([namem,agem,ageh])
 
-    # nans = np.sum(np.array([rankm, rankh]) == 'nan')
-    # if nans == 1:
-    #     print(f'{namem} map: {rankm} haaretz: {rankh}')
-    #     missmatch.append(f'{namem} map: {rankm} haaretz: {rankh}')
-    # elif nans ==0 and rankm != rankh:
-    #     print(f'{namem} map: {rankm} haaretz: {rankh}')
-    #     missmatch.append(f'{namem} map: {rankm} haaretz: {rankh}')
-
-#
-# coo = pd.read_excel('data/deaths_by_loc.xlsx', 'coo')
-# names = pd.read_excel('data/deaths_by_loc.xlsx', 'export')
-# # names['location'] = names['location'].str.replace('?', 'בבירור')
-# # coo = pd.read_csv('data/deaths_by_loc.csv')
-# center = [coo['lat'].mean(), coo['long'].mean()]
-
-#
-# ##
-# fixed = pd.read_excel('data/tmp.xlsx')
-# blank = fixed[fixed['haaretz_row'].isnull()]
-# blank.to_csv('data/tmp_nohaa.csv')
-# imiss = [x for x in range(len(haa)) if x not in fixed['haaretz_row'].values]
-# missed = haa.iloc[np.array(imiss)-2]
-# missed = missed[missed['death_date'].values < '2023-10-10']
-# missed.to_csv('data/tmp_unacc.csv')
-# haarow = np.where(~fixed['haaretz_row'].isnull())[0]
-# for ii in range(len(haarow)):
-#     # print(haa['name'][fixed['haaretz_row'][haarow[ii]]-2]+' '+fixed['fullName'][haarow[ii]])
-#     haa.at[fixed['haaretz_row'][haarow[ii]] - 2, 'map_row'] = haarow[ii]+2
+missmatch = []
+for ii in range(2, len(names) + 2):
+    haarow = np.where(haa['map_row'] == ii)[0][0]
+    namem = names['fullName'][ii - 2]
+    agem = names['age'][ii - 2]
+    ageh = haa['age'][haarow]
+    # jdf = haa['idf_row'][haarow]
+    # if ~np.isnan(jdf):  # type(jdf) == np.float64:
+    #     agei = idf['age'][jdf - 2]
+    if np.abs(agem - ageh) > 1:
+        missmatch.append(f'{namem} map: {agem} haa: {ageh}')
+        haa.at[haarow, 'age'] = agem
+haa.to_csv('data/deaths_haaretz+.csv', index=False)
