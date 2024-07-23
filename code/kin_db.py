cb0ecabe1d9b24783937c134f2af3c15bf3cf4cb
yuval <yuval@lenovo.2020>
2024-07-23 21:56:44 +0300

    el atrash duplicate

--- code/kin_db.py
+++ code/kin_db.py
@@ -1,18 +1,17 @@
-
 import pandas as pd
-import os
 import numpy as np
 
 db = pd.read_excel('~/Documents/oct7database.xlsx', 'Data')
 residence = db['residence'].values
-evloc = db['מקום האירוע'].values
+evloc = np.array([str(x).split(';')[0] for x in db['מקום האירוע'].values])
 keep = []
 for ii in range(len(db)):
     name = db['שם משפחה'][ii]
     namesp = name.split(' ')
     matches = False
-    candidates = np.where((residence == db['residence'][ii]) | (evloc == db['מקום האירוע']))[0]
+    candidates = np.where((residence == db['residence'][ii]) | (evloc == db['מקום האירוע'][ii]))[0]
     for jj in candidates:
+        # if jj != ii:
         if jj not in keep and jj != ii:
             compare = db['שם משפחה'][jj]
             keepit = False
@@ -20,7 +19,7 @@ for ii in range(len(db)):
                 keepit = True
                 matches = True
             else:
-                compare = [x for x in compare if len(x) > 2]
+                compare = [x for x in compare.replace('-', ' ').split(' ') if len(x) > 2]
                 if len(compare):
                     for comp in compare:
                         # cond = (len(compare) == 1 | len(comp) > 2) and (comp in name)
@@ -39,20 +38,23 @@ for ii in range(len(db)):
 
 ##
 # xlsx = pd.read_excel('~/Documents/oct7database.xlsx', 'Data')
-df = pd.DataFrame(columns=['pid','name','residence','event location','event date', 'status'])
+df = pd.DataFrame(columns=['pid', 'name', 'first name', 'last name',
+                           'residence', 'event location', 'event date', 'status'])
 for kk in range(len(keep)):
     pid = db['pid'][keep[kk]]
     name = (db['שם פרטי'][keep[kk]] + ';' +
             str(db['שם נוסף'][keep[kk]]) + ';' +
             db['שם משפחה'][keep[kk]]
             ).replace('nan', '').replace(';;', ' ').replace(';', ' ')
+    first = db['שם פרטי'][keep[kk]]
+    last = db['שם משפחה'][keep[kk]]
     if db['pid'][keep[kk]] != pid:
         raise Exception('pid not the same for ' + str(keep[kk]))
     residence = db['residence'][keep[kk]]
-    loc = db['מקום האירוע'][keep[kk]]
+    loc = db['מקום האירוע'][keep[kk]].split(';')[0]
     date = str(db['Event date'][keep[kk]])[:10]
     status = db['Status (oct7map)'][keep[kk]]
-    df.loc[len(df)] = [pid, name, residence, loc, date, status]
+    df.loc[len(df)] = [pid, name, first, last, residence, loc, date, status]
 
 df.to_excel('~/Documents/names_by_common_location.xlsx', index=False)
 
