1a6f62b35c62cb70f1b782758dc684928181a5c2
yuval <yuval@lenovo.2020>
2024-08-04 12:18:01 +0300

    locations

--- code/export_coordinates.py
+++ code/export_coordinates.py
@@ -4,6 +4,7 @@ consider using war23_map2db.py if something changed in oct_7_9.csv
 test with war23_db_tests.py
 push
 refresh and download database as excel to ~/Documents/oct7database.xlsx
+run this code
 
 The code should make 3 columns of locations. oct7map, db by place name, and db coordinates. Check for large differences
 """
@@ -58,6 +59,10 @@ sublocheb = {'אשקלון; בי"ח ברזילי': [31.6632444309682, 34.5578359
              'סמוך למיגונית בצומת גמה (צפון)': [31.381307046724753, 34.44754169695219],
              'עזה; ביה"ח שיפא': [31.399963, 34.474210],
              'עזה; מבנה סמוך לביה"ח שיפא': [31.399963, 34.474210],
+             'דיר אל בלח; בי"ח חללי אל אקצא': [31.42097888989603, 34.35960647838112],
+             'חאן יונס; ביה"ח נאצר': [31.351538612807232, 34.292552168121006],
+             'מצרים; אלכסנדריה': [31.182732054836766, 29.896308654768227],
+             "ג’נין; מחנה הפליטים": [32.461362867968454, 35.28754210285402],
              'עזה; מסגד סמוך לביה"ח שיפא': [31.399963, 34.474210],
              'פסטיבל נובה; אמבולנס': [31.397351, 34.469556],
              'פסטיבל נובה; במה מרכזית': [31.397771, 34.469951],
@@ -101,6 +106,8 @@ for ii in range(len(pids)):
                 locations.at[ii, 'death coo'] = coo
             else:
                 print(f'no coo for {loc79}')
+                if loc79 == "ג'נין; מחנה הפליטים":
+                    print('x')
         else:
             li = np.where(maploc['name'] == loc79)[0]
             if len(li):
@@ -171,132 +178,3 @@ for ii in range(len(locations)):
         locations.at[ii, cols[0]] = geodif(ii, cols[1], cols[2])
 
 locations.to_csv('~/Documents/locations.csv', index=False)
-##
-#
-# locs = pd.DataFrame(columns=['pid', 'oct_7_9_name', 'oct_7_9_loc', 'oct7map_loc'])
-# # locs['oct_7_9_loc'] = map['location']
-# for ii in range(len(pids)):
-#     pid = pids[ii]
-#     row = np.where(map7['pid'] == pid)[0][0]
-#     loc = map7['location'][row]
-#     sl = map7['sublocation'][row]
-#     if type(sl) == str:
-#         loc = loc+'; '+sl
-#     locs.at[ii, 'oct7map_loc'] = loc
-#     locs.at[ii, 'oct7map_coo'] = map7['geotag'][row]
-#     locs.at[ii, 'pid'] = pid
-#
-# ## complete coordinates for missing geotag based on sublocation
-# # subloc = np.unique(locs['oct7map_subloc'][locs['oct7map_coo'].isnull()].values.astype(str))
-#
-# for ii in range(len(locs)):
-#     pid = locs['pid'][ii]
-#     coo = str(locs['oct7map_coo'][ii])
-#     row = np.where(map7['pid'] == pid)[0][0]
-#     geotag = map7['geotag'][row]
-#     sl = map7['sublocation'][row]
-#     lc = map7['location'][row]
-#     if type(geotag) == str and geotag[:2] == '31':
-#         locs.at[ii, 'oct7map_coo'] = geotag
-#     elif str(sl) not in ['nan', 'None']:
-#         locs.at[ii, 'oct7map_coo'] = str(subloc[sl])[1: -1]
-#     else:
-#         lrow = np.where(maploc['oct7map'].values == lc)[0]
-#         if len(lrow) == 1:
-#             geotag = str(maploc['lat'][lrow[0]]) + ', ' + str(maploc['long'][lrow[0]])
-#             locs.at[ii, 'oct7map_coo'] = geotag
-#
-#
-# ## complete missing data based on location
-# # maploc = pd.read_csv('data/deaths_by_loc.csv')
-# # locrow = np.where(~maploc['oct7map'].isnull())[0]
-# # for lr in locrow:
-# #     trow = np.where(locs['oct7map_loc'].values == maploc['oct7map'][lr])[0]
-# #     for tr in trow:
-# #         if str(locs['oct7map_coo'][tr])[:2] != '31':
-# #             locs.at[tr, 'oct7map_coo'] = str(maploc['lat'][lr])+', '+str(maploc['long'][lr])
-#
-# ## sublocations not represented in oct_7_9
-# other = {'The pensioners bus in Sderot': '31.522808876615766, 34.59568825785523',
-#          'Sderot Police Station': '31.522808876615766, 34.59568825785523',
-#          'Erez': '31.55999246335105, 34.56505148304494',
-#          'Kerem Shalom': '31.228310751744882, 34.28445082924824',
-#          'COGAT Base': '31.55987823169991, 34.54622846569096'}
-#
-# for othr in list(other.keys()):
-#     trow = np.where(locs['oct7map_loc'].values == othr)[0]
-#     for tr in trow:
-#         if str(locs['oct7map_coo'][tr])[:2] != '31':
-#             locs.at[tr, 'oct7map_coo'] = other[othr]
-# ##
-# names = pd.read_csv('data/oct_7_9.csv')
-# names = group_locs(names)
-# for ii in range(len(locs)):
-#     row = np.where(names['pid'] == locs['pid'][ii])[0][0]
-#     lrow = np.where(maploc['name'].values == names['location'][row].split(';')[0])[0]
-#     if len(lrow) == 0:
-#         raise Exception(ii)
-#     locs.at[ii, 'oct_7_9_name'] = names['fullName'][row]
-#     locs.at[ii, 'oct_7_9_loc'] = names['location'][row]
-#     geotag = str(maploc['lat'][lrow[0]]) + ', ' + str(maploc['long'][lrow[0]])
-#     locs.at[ii, 'oct_7_9_coo'] = geotag
-# locs = locs[~locs['oct7map_coo'].isnull()]
-# locs.reset_index(inplace=True, drop=True)
-# locs.to_csv('data/tmp_locs.csv', index=False)
-# ## compute distance
-#
-# for ii in range(len(locs)):
-#     coo79 = [float(x) for x in locs['oct_7_9_coo'][ii].split(',')]
-#     coo7 = [float(x) for x in locs['oct7map_coo'][ii].split(',')]
-#     dif = geodesic(coo7, coo79).km
-#     locs.at[ii, 'dif'] = np.round(dif, 1)
-# locs = locs.sort_values(['dif', 'oct7map_loc'], ignore_index=True, ascending=[False, True])
-# locs.to_csv('data/tmp_locs.csv', index=False)
-#
-# ##
-# difs = locs.copy()
-# for exc in ['שיפא', 'חאן', 'רצועת עזה', 'באלי', 'נוסייר']:
-#     difs = difs[~difs['oct_7_9_loc'].str.contains(exc)]
-# # difs = difs[~difs['dif'].isnull()]
-# # difs = difs.sort_values('dif', ascending=False, ignore_index=True)
-# difs = difs[difs['dif'] > 1]
-# difs.to_csv('data/tmp_dif.csv', index=False)
-# ##
-# # locs = pd.read_csv('data/tmp_locs.csv')
-# # est = locs['oct7map_est'].values.astype(str)
-# # est = np.array([x.strip() for x in est])
-# # est[est == 'nan'] = 'None'
-# # est[est == ''] = 'None'
-# # estu = np.unique(est)
-# # dif = locs['dif'].values
-# # co = ['k*', '.c', '.b', '.g', '.k', '.r']
-# # plt.figure()
-# # for ig in range(len(estu)):
-# #     x = np.where(est == estu[ig])[0]
-# #     plt.plot(x, dif[x], co[ig], label=estu[ig])
-# # plt.legend()
-# # plt.xlabel('oct_7_9 row in table')
-# # plt.ylabel('difference in km')
-# # plt.grid()
-# # plt.title('distance by estimate type')
-#
-#
-#
-# # nameh = map7['hebrew_name'].values
-# # namee = map7['name'].values  # [(map7['status'].values == 'Murdered') | (map7['status'].values == 'Killed on duty')]
-# # map = pd.read_csv('data/oct_7_9.csv')
-# # name = map['eng'].values
-# # ##
-# # # missing = [x.strip() for x in namee if x.strip() not in name]
-# # missing = []
-# # for ii in range(len(name)):
-# #     row = np.where(namee == name[ii])[0]
-# #     if len(row) == 1:
-# #         map.at[ii, 'oct7map_pid'] = map7['pid'][row[0]]
-# #     else:
-# #         map.at[ii, 'oct7map_pid'] = 0
-# #         missing.append(ii)
-# # map['oct7map_pid'] = map['oct7map_pid'].values.astype(int)
-# # map.to_excel('/home/innereye/Documents/pid.xlsx', index=False)
-#
-# ##
