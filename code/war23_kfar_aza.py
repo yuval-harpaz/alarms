0cb6de2e186efbc5167d730043af1064fe690323
yuval <yuval@lenovo.2020>
2024-08-24 13:15:56 +0300

    tamra

--- code/war23_kfar_aza.py
+++ code/war23_kfar_aza.py
@@ -19,3 +19,12 @@ for ii in range(0, len(txt), 3):
     df.loc[int(ii/3)] = [txt[ii+2], txt[ii+1].replace('(','').replace(')','')]
 
 df.to_csv('~/Documents/kacoo.csv', index=False)
+
+##
+db = pd.read_csv('~/Documents/oct7database - Data.csv')
+ka = pd.read_csv('~/Documents/כפר עזה - data.csv')
+for ii in range(len(ka)):
+    row = np.where(db['pid'].values == ka['pid'][ii])[0][0]
+    if str(ka['visit'][ii]) != 'nan':
+        db.at[row, 'event_coordinates'] = ka['visit'][ii]
+db.to_csv('~/Documents/oct7database.csv', index=False)
