e122f996f3e21f0524d702ac3cf85649762231e9
yuval <yuval@lenovo.2020>
2024-08-28 16:25:24 +0300

    map_update

--- code/scrap.py
+++ code/scrap.py
@@ -12,9 +12,14 @@ if os.path.isdir(local):
 
 
 db = pd.read_csv('data/oct7database.csv')
-changes = pd.read_csv('~/Documents/changes.csv')
-ich = np.where(changes['fixed'].str.lower() == 'v')[0]
-for ii in ich:
-    row = np.where(db['pid'].values == changes['pid'][ii])[0][0]
-    db.at[row, 'Role'] = changes['Role'][ii]
-db.to_csv('data/oct7database.csv', index=False)
+did = db['pid'].values[db['Status'].str.contains('killed') & db['מקום האירוע'].str.contains('פסטיבל נובה')]
+map = df = pd.read_csv('data/oct_7_9.csv')
+mid = map['pid'].values[map['location'].str.contains('פסטיבל נובה')]
+idif = [id for id in mid if id not in did]  # guy iluz 775
+idif0 = [id for id in did if id not in mid]  # ofir testa 139
+# changes = pd.read_csv('~/Documents/changes.csv')
+# ich = np.where(changes['fixed'].str.lower() == 'v')[0]
+# for ii in ich:
+#     row = np.where(db['pid'].values == changes['pid'][ii])[0][0]
+#     db.at[row, 'Role'] = changes['Role'][ii]
+# db.to_csv('data/oct7database.csv', index=False)
