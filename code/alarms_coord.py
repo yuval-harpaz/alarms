4decce4575ce98409a2d7e5680c59c0b5b64dcaa
yuval <yuval@lenovo.2020>
2024-09-18 19:57:04 +0300

    cities

--- code/alarms_coord.py
+++ code/alarms_coord.py
@@ -10,9 +10,11 @@ def update_coord(latest=None, coord_file='data/coord.csv'):
         os.chdir(local)
         with open('/home/innereye/alarms/oath.txt') as f:
             oauth = f.readlines()[0][:-1]
+        with open('/home/innereye/alarms/.txt') as f:
+            cities_url = f.readlines()[1][:-1]
     else:
         oauth = os.environ['OAuth']
-
+        cities_url = os.environ['cities_url']
     def get_coordinates(city_name):
         # city_name = city_name + ', ישראל'
         geocoder_url = f'https://maps.googleapis.com/maps/api/geocode/json?address={city_name}&key={oauth}&language=iw'
@@ -23,6 +25,17 @@ def update_coord(latest=None, coord_file='data/coord.csv'):
         else:
             lat = geocoding_result['results'][0]['geometry']['location']['lat']
             long = geocoding_result['results'][0]['geometry']['location']['lng']
+        return lat, long
+
+    def get_coordinates_zofar(city_name):
+        # cities = pd.read_json(cities_url)
+        cities = requests.get(cities_url).json()
+        if city_name in cities['cities'].keys():
+            lat = cities['cities'][city_name]['lat']
+            long = cities['cities'][city_name]['lng']
+        else:
+            lat = 0
+            long = 0
         return lat, long
 
     coo = pd.read_csv(coord_file)
@@ -46,7 +59,9 @@ def update_coord(latest=None, coord_file='data/coord.csv'):
     if len(bad) > 0:
         print(f'fixing {len(bad)} locations')
         for bd in bad:
-            lat, long = get_coordinates(coo['loc'][bd])
+            lat, long = get_coordinates_zofar(coo['loc'][bd])
+            if lat == 0:
+                lat, long = get_coordinates(coo['loc'][bd])
             coo.loc[bd] = [coo['loc'][bd], lat, long]
         coo.sort_values('loc', inplace=True)
         coo.to_csv(coord_file, sep=',', index=False)
