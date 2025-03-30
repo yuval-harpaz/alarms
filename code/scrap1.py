9e8c7b9892da23cf2e70e9f75870828bd7eed87f
yuval <yuval@lenovo.2020>
2025-03-30 23:07:28 +0300

    kuchla

--- code/scrap1.py
+++ code/scrap1.py
@@ -2,42 +2,16 @@ import pandas as pd
 import os
 import numpy as np
 import sys
-import json
-# sys.path.append('code')
 
-# df = pd.read_json('/home/innereye/Downloads/Telegram Desktop/ChatExport_2024-10-18/result.json')
-file = '/home/innereye/Downloads/Telegram Desktop/ChatExport_2024-10-18/result.json'
-f = open(file)
-data = json.load(f)
-f.close()
-# df = pd.DataFrame(columns=['date', 'text'])
-# for ii in range(len(data['messages'])):
-#     date = data['messages'][ii]['date'].replace('T', ' ')
-#     text = data['messages'][ii]['text']
-#     df.at[ii, 'date'] = date
-#     df.at[ii, 'text'] = text
-#     print(ii)
-# df = df[df['date'] > '2023-10-07']
-# df.to_excel('~/Documents/tsahal.xlsx')
-
-
-messages = []
-for message in data['messages']:
-    if 'date' in message and 'text' in message:
-        # Handle both cases where 'text' is a string or a list (in case of formatting markers)
-        if isinstance(message['text'], list):
-            text = ''.join([str(item) if isinstance(item, str) else '' for item in message['text']])
-        else:
-            text = message['text']
-        text = text.replace('דובר צה״ל:', '').replace('דובר צה"ל:', '').strip()
-        messages.append({
-            'date': message['date'].replace('T', ' '),
-            'text': text
-        })
-
-# Create a DataFrame and export it to Excel
-df1 = pd.DataFrame(messages)
-df1 = df1[df1['date'] > '2023-10-07']
-df1.to_excel('~/Documents/tsahal_cg.xlsx', index=False)
-fa = df1[df1['text'].str.contains('שווא')]
-fa.to_csv('~/Documents/fa.csv', index=False)
+df = pd.read_csv('data/oct7database.csv')
+notathome = [str(df['Residence'][x]) not in str(df['מקום האירוע'][x]) for x in range(len(df))]
+noparty = np.array(df['Party'].isnull())
+oct7 = df['Death date'].values == '2023-10-07'
+citizen = df['Role'].values == 'אזרח'
+res = np.unique(df['Residence'][~df['Residence'].isnull()])
+nores = np.array([str(x).split(';')[0] not in res for x in df['מקום האירוע'].values])
+nozikim = np.array(['זיקים' not in str(x) for x in df['מקום האירוע']])
+candidates = oct7 & notathome & noparty & citizen & nores & nozikim
+np.unique(df['מקום האירוע'][candidates])
+select = df[candidates]
+select.to_csv('~/Downloads/party_candidates.csv', index=False)
