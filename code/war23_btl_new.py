ab68ee2ff5f6757344a50381652c1d1cd1fb85a2
yuval <yuval@lenovo.2020>
2025-04-21 09:36:02 +0300

    additional

--- code/war23_btl_new.py
+++ code/war23_btl_new.py
@@ -25,7 +25,7 @@ ID = ID + [int(id.split('.aspx?ID=')[-1]) for id in add['הנצחה'].values if 
 maxID = np.max(ID)
 minID = np.min(ID)
 # bad = [43753]
-bad = [44144, 44172, 44200, 44210, 44251]
+bad = [44144, 44172, 44200, 44210, 44251, 44270, 44271, 44272]
 browser = webdriver.Chrome()
 difs = []
 count = -1
