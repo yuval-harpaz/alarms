f3d77c0a9ac40a21ca04464ad8b449f416d1999f
yuval <yuval@lenovo.2020>
2025-05-03 23:39:24 +0300

    dogs

--- code/war23_dogs.py
+++ code/war23_dogs.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import sys
 import folium
-import requests
+# import requests
 
 local = '/home/innereye/alarms/'
 os.chdir(local)
@@ -13,6 +13,15 @@ fn = 'data/dogs.csv'
 df = pd.read_csv(fn)
 df['שם יישוב'] = df['שם יישוב'].str.strip()
 df.to_csv(fn, index=False)
+status = []
+for ii in range(len(df)):
+    missing = df['נעדר'][ii]
+    if str(missing) == 'nan':
+        status.append('נרצח')
+    elif ' ששב' in missing or ' שחזר' in missing:
+        status.append('חטוף ששב')
+    else:
+        status.append('נעדר')
 # df = pd.read_excel('~/Documents/טבלת אקסל סופית של כלבים שנרצחו ב7.10 נכון ל 6.10.24.xlsx')
 coo = pd.read_csv('data/coord.csv')
 locs = df['שם יישוב'].values
@@ -39,6 +48,10 @@ folium.TileLayer('cartodbpositron').add_to(map)
 now = np.datetime64('now', 'ns')
 nowisr = pd.to_datetime(now, utc=True, unit='s').astimezone(tz='Israel')
 nowstr = str(nowisr)[:16].replace('T', ' ')
+status = np.array(status)
+missing = np.sum(status == 'נעדר')
+killed = np.sum(status == 'נרצח')
+returned = np.sum(status == 'חטוף ששב')
 title_html = f'''
              <h3 align="center" style="font-size:20px">
              כלבים שנרצחו בשבעה באוק' או שנעדרים מאז.
@@ -46,8 +59,9 @@ title_html = f'''
                <a href="https://docs.google.com/spreadsheets/d/1jOXg2FVlNapTrSAbRdUyqwaEik-sjz1PU8pGeSR-wMQ/edit?usp=sharing" target="_blank">            
              טבלת נתונים
              </a>
-             <br>
             <h3 align="center" style="font-size:12px">
+            בטבח נרצחו {killed} כלבים, {returned} נחטפו ושבו ויש {missing} נעדרים
+            <br>
             כל היודע מידע נוסף על כלבים שנרצחו או נעדרים בשל אירועי ה-7/10, מתבקש לפנות לתמי בר-יוסף 
             <a href="mailto:tammybj@gmail.com" target="_blank">במייל</a> או באתר <a href="https://www.humandogreaserch.org/">המרכז לחקר יחסי אדם וכלב בישראל ובשואה</c>
              </a></h3>
@@ -65,7 +79,7 @@ for iloc in range(len(locu)):
         tip = f'{locu[iloc]}  {size[iloc]}<br>'
         rows_dogs = np.where(locs == locu[iloc])[0]
         for row_dog in rows_dogs:
-            tip += f"{df['שם הכלב'][row_dog]}, {df['גיל'][row_dog]}, ({df['שם משפחה'][row_dog]})<br>"
+            tip += f"{df['שם הכלב'][row_dog]}, {df['גיל'][row_dog]}, {status[row_dog]}, ({df['שם משפחה'][row_dog]})<br>"
         tip = tip[:-4]
         radius = (size[iloc]/np.pi)**0.5
         folium.Circle(location=[lat, long],
