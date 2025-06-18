79d8f42d59b81048ebbe2180f8b84f58b0a79da0
yuval <yuval@lenovo.2020>
2025-06-18 21:52:01 +0300

    haaretz

--- code/war25_count.py
+++ code/war25_count.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import requests
+import matplotlib.pyplot as plt
 with open('/home/innereye/alarms/.txt') as f:
     cities_url = f.readlines()[1][:-1]
 cities = requests.get(cities_url).json()
@@ -20,3 +21,52 @@ for des in np.unique(iran['description']):
     if "רקטות" in des:
         n = n - allover + allover * n_cities
     print(f"{des}: {n}")
+
+df = pd.read_csv('data/alarms.csv')
+df = df[df['time'] > '2023-10-07']
+country = ['Gaza','Lebanon','Yemen','Iran']
+count = []
+for ii in range(4):
+    count.append(np.sum(df['origin'].values == country[ii]))
+
+plt.figure()
+# Set up grid first with lower zorder
+plt.grid(axis='y', zorder=0)
+# Draw bars with higher zorder to appear in front
+for ii in range(4):
+    plt.bar(ii, count[ii], 0.667, color=['r','g','brown','k'][ii], zorder=3)
+for ii in range(4):
+    formatted_count = f"{count[ii]:,}"
+    plt.text(ii, 1000, formatted_count, ha='center', va='bottom', color='w')
+plt.xticks(range(4), country)
+plt.ylabel('Number of Alarms')
+plt.title('Alarms by Origin')
+plt.tight_layout()
+
+# check how many alarm events there where by grouping unique times
+df = pd.read_csv('data/alarms.csv')
+df = df[df['time'] > '2023-10-07']
+df = df[df['description'] == 'ירי רקטות וטילים']
+time = df['time'].values
+time = [t[:-3] for t in time]
+df['time'] = time
+country = ['Gaza','Lebanon','Yemen','Iran']
+count = []
+for ii in range(4):
+    df_country = df[df['origin'].values == country[ii]]
+    count.append(len(np.unique(df_country['time'])))
+
+
+plt.figure()
+# Set up grid first with lower zorder
+plt.grid(axis='y', zorder=0)
+# Draw bars with higher zorder to appear in front
+for ii in range(4):
+    plt.bar(ii, count[ii], 0.667, color=['r','g','brown','k'][ii], zorder=3)
+for ii in range(4):
+    formatted_count = f"{count[ii]:,}"
+    plt.text(ii, 150, formatted_count, ha='center', va='bottom', color='k')
+plt.xticks(range(4), country)
+plt.ylabel('Number of Events')
+plt.title('Alarm Events by Origin')
+plt.tight_layout()
