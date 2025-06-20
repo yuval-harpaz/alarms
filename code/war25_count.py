904ceab166a1a16effe5adfa3107e7520d61f0d8
yuval <yuval@lenovo.2020>
2025-06-20 22:20:32 +0300

    bug

--- code/war25_count.py
+++ code/war25_count.py
@@ -24,10 +24,14 @@ for des in np.unique(iran['description']):
 
 df = pd.read_csv('data/alarms.csv')
 df = df[df['time'] > '2023-10-07']
+n_allover = len(np.unique(df['cities']))
 country = ['Gaza','Lebanon','Yemen','Iran']
 count = []
 for ii in range(4):
-    count.append(np.sum(df['origin'].values == country[ii]))
+    icountry = np.where(df['origin'].values == country[ii])[0]
+    df_country = df.iloc[icountry]
+    allover = sum(df_country['cities'] == 'ברחבי הארץ')
+    count.append(len(df_country) - allover + allover * n_allover)
 
 plt.figure()
 # Set up grid first with lower zorder
@@ -42,19 +46,34 @@ plt.xticks(range(4), country)
 plt.ylabel('Number of Alarms')
 plt.title('Alarms by Origin')
 plt.tight_layout()
+plt.show(block=True)
 
 # check how many alarm events there where by grouping unique times
 df = pd.read_csv('data/alarms.csv')
 df = df[df['time'] > '2023-10-07']
 df = df[df['description'] == 'ירי רקטות וטילים']
-time = df['time'].values
-time = [t[:-3] for t in time]
-df['time'] = time
 country = ['Gaza','Lebanon','Yemen','Iran']
+distance = 15 # minutes
 count = []
+avg = []
+# plt.figure()
 for ii in range(4):
-    df_country = df[df['origin'].values == country[ii]]
-    count.append(len(np.unique(df_country['time'])))
+    icountry = np.where(df['origin'].values == country[ii])[0]
+    df_country = df.iloc[icountry]
+    time = pd.to_datetime(df_country['time'], format='%Y-%m-%d %H:%M:%S')
+    diff = time.diff().dropna()
+    diff = diff.dt.total_seconds().values / 60  # Convert to minutes
+    diff = diff[diff > distance]
+    allover = sum(df_country['cities'] == 'ברחבי הארץ')
+    count.append(len(diff) + 1)  # +1 to count the first event
+    # if allover:
+    #     print(f"Allover: {allover} events in {country[ii]}")
+    avg.append(int(np.round((len(time) + allover * n_allover - allover) / count[ii])))  # Average event size
+    print(f"{country[ii]}: {count[ii]} events, avg size: {avg[ii]}")
+    # plt.subplot(2, 2, ii+1)
+    # h = plt.hist(diff, bins=100, color=['r','g','brown','k'][ii], zorder=3)
+    # plt.title(country[ii])
+
 
 
 plt.figure()
@@ -68,5 +87,6 @@ for ii in range(4):
     plt.text(ii, 150, formatted_count, ha='center', va='bottom', color='k')
 plt.xticks(range(4), country)
 plt.ylabel('Number of Events')
-plt.title('Alarm Events by Origin')
+plt.title(f'Alarm Events (>{distance}min quiet) by Origin')
 plt.tight_layout()
+plt.show(block=True)
