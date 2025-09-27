b9a44077cd71fdfcd1879480916731c8d4a2b194
yuval <yuval@lenovo.2020>
2025-09-27 23:41:03 +0300

    tsv

--- code/scrap.py
+++ code/scrap.py
@@ -2,23 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 
-
-uptodate = pd.read_csv('~/Documents/NLI 4 oct7database - manual.csv', 
-                       dtype={'NLI': str, 'suggestion': str})
-db = pd.read_csv('data/oct7database.csv', dtype={'הספריה הלאומית': str})
-# check pid are the same
-min_len = min([len(db), len(uptodate)])
-if np.mean(db['pid'].values[:min_len] == uptodate['pid'].values[:min_len]) != 1:
-    first_unequal = np.where(db['pid'].values[:min_len] != uptodate['pid'].values[:min_len])[0][0]
-    print('pid issues, first one for pid ' + str(db['pid'].values[first_unequal]) + ' at index ')
-    raise ValueError('pid mismatch between oct7database.csv and NLI 4 oct7database - manual.csv')
-
-for row, pid in enumerate(uptodate['pid'].values[:min_len]):
-    # row = np.where(db['pid'].values == pid)[0][0]
-    nli_value = str(uptodate['suggestion'].values[row])
-    db_value = str(db['הספריה הלאומית'].values[row])
-    harpaz_id = uptodate['harpaz_id'][row]
-    if pid == harpaz_id and nli_value[0] == '9' and db_value[:3] == 'nan':
-        print(f"pid {pid} has harpaz_id {harpaz_id} and NLI value {nli_value} but no NLI ID in DB")
-        db.at[row, 'הספריה הלאומית'] = nli_value
-db.to_csv('data/oct7database.csv', index=False)
+areas = pd.read_csv('data/coord_area.csv')
+areas_tab = pd.read_csv('data/polygons.tsv', sep='\t', header=None, index_col=None)
+for ii in range(len(areas)):
+    next_row = len(areas_tab)
+    new = areas.loc[ii].values
+    new = [new[0]] + [n.strip() for n in new[1].split(';')]
+    until = len(new)
+    for jj in range(until):
+        areas_tab.at[next_row, jj] = new[jj]
+areas_tab.to_csv('data/coord_area.tsv', sep='\t', header=None, index=None)
