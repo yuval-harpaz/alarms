272c6a513913dcfa88f97f5a6e2d3a11db9d79cb
yuval <yuval@lenovo.2020>
2025-11-28 23:21:31 +0200

    no_folium

--- code/alarms3.py
+++ code/alarms3.py
@@ -82,126 +82,128 @@ if islocal:
 from alarms_coord import update_coord
 update_coord()
 
+if False:
+    prev = prev[prev['threat'] == 0]
+    prev = prev.reset_index(drop=True)
+    yyyy = np.array([int(str(date)[:4]) for date in prev['time']])
+    mm = np.array([int(str(date)[5:7]) for date in prev['time']])
+    now = np.datetime64('now', 'ns')
+    nowisr = pd.to_datetime(now, utc=True, unit='s').astimezone(tz='Israel')
+    nowstr = str(nowisr)[:16].replace('T', ' ')
+    dt = pd.to_datetime(prev['time']).to_numpy()
+    dif = np.datetime64(nowstr) - dt
+    dif_sec = dif.astype('timedelta64[s]').astype(float)
+    dif_days = dif_sec / 60 ** 2 / 24
+    past_24h = dif_days <= 1
+    past_7d = dif_days <= 7
+    current_year = datetime.now().year
+    # n = []
+    # mmyy = []
+    # for year in range(2019, current_year + 1):
+    #     for month in range(1, 13):
+    #         idx = (yyyy == year) & (mm == month)
+    #         n.append(len(np.unique(prev['id'][idx])))
+    #         mmyy.append(str(month)+'.'+str(year))
 
-prev = prev[prev['threat'] == 0]
-prev = prev.reset_index(drop=True)
-yyyy = np.array([int(str(date)[:4]) for date in prev['time']])
-mm = np.array([int(str(date)[5:7]) for date in prev['time']])
-now = np.datetime64('now', 'ns')
-nowisr = pd.to_datetime(now, utc=True, unit='s').astimezone(tz='Israel')
-nowstr = str(nowisr)[:16].replace('T', ' ')
-dt = pd.to_datetime(prev['time']).to_numpy()
-dif = np.datetime64(nowstr) - dt
-dif_sec = dif.astype('timedelta64[s]').astype(float)
-dif_days = dif_sec / 60 ** 2 / 24
-past_24h = dif_days <= 1
-past_7d = dif_days <= 7
-current_year = datetime.now().year
-# n = []
-# mmyy = []
-# for year in range(2019, current_year + 1):
-#     for month in range(1, 13):
-#         idx = (yyyy == year) & (mm == month)
-#         n.append(len(np.unique(prev['id'][idx])))
-#         mmyy.append(str(month)+'.'+str(year))
 
+    # fig = px.bar(x=mmyy, y=n, log_y=True)
+    # # fig = px.bar(prev, y=n, x='date',log_y=True)
+    # html = fig.to_html()
+    # file = open('docs/rockets_timeline.html', 'w')
+    # a = file.write(html)
+    # file.close()
+    ##
 
-# fig = px.bar(x=mmyy, y=n, log_y=True)
-# # fig = px.bar(prev, y=n, x='date',log_y=True)
-# html = fig.to_html()
-# file = open('docs/rockets_timeline.html', 'w')
-# a = file.write(html)
-# file.close()
-##
-# Make map
-title_html = f'''
-             <h3 align="center" style="font-size:16px"><b>Rocket alarms in Israel since July 2019, data from <a href="https://www.oref.org.il" target="_blank">THE NATIONAL EMERGENCY PORTAL</a>
-             via <a href="https://www.tzevaadom.co.il/" target="_blank">צבע אדום</a>. last checked: {nowstr}</b></h3>
-             '''
+    # Make map
+    title_html = f'''
+                <h3 align="center" style="font-size:16px"><b>Rocket alarms in Israel since July 2019, data from <a href="https://www.oref.org.il" target="_blank">THE NATIONAL EMERGENCY PORTAL</a>
+                via <a href="https://www.tzevaadom.co.il/" target="_blank">צבע אדום</a>. last checked: {nowstr}</b></h3>
+                '''
 
-gnames = [str(year) for year in range(2019, current_year + 1)] + ['7 days', '24 h']
-# gnames = ['2019', '2020', '2021', '2022', '2023', '2024', '7 days', '24 h']
-# co = [[0.8, 0.8, 0.8], [0.25, 0.25, 1.0], [0.25, 0.9, 0.8], [0.25, 1, 0.25], [0.75, 0.75, 0.25], [0.82, 0.5, 0.35],
-#       [1.0, 0.25, 0.25], [0, 0, 0]]
-co = np.array(colormaps['tab20'].colors)
-idx = np.concatenate([np.arange(0, 20, 2), np.arange(1, 21, 2)])
-co = co[idx, :]
-ired = np.where(co[:, 0] == 0.8392156862745098)[0][0]
-red = co[ired, :]
-co = np.concatenate([co[:ired, :], co[ired+1:, :]])
-co = np.concatenate([co[:len(gnames)-2], [red, [0, 0, 0]]])
-chex = []
-for c in co:
-    chex.append(colors.to_hex(c))
-lgd_txt = '<span style="color: {col};">{txt}</span>'
-grp = []
-for ic, gn in enumerate(gnames):
-    grp.append(folium.FeatureGroup(name=lgd_txt.format(txt=gn, col=chex[ic])))
+    gnames = [str(year) for year in range(2019, current_year + 1)] + ['7 days', '24 h']
+    # gnames = ['2019', '2020', '2021', '2022', '2023', '2024', '7 days', '24 h']
+    # co = [[0.8, 0.8, 0.8], [0.25, 0.25, 1.0], [0.25, 0.9, 0.8], [0.25, 1, 0.25], [0.75, 0.75, 0.25], [0.82, 0.5, 0.35],
+    #       [1.0, 0.25, 0.25], [0, 0, 0]]
+    co = np.array(colormaps['tab20'].colors)
+    idx = np.concatenate([np.arange(0, 20, 2), np.arange(1, 21, 2)])
+    co = co[idx, :]
+    ired = np.where(co[:, 0] == 0.8392156862745098)[0][0]
+    red = co[ired, :]
+    co = np.concatenate([co[:ired, :], co[ired+1:, :]])
+    co = np.concatenate([co[:len(gnames)-2], [red, [0, 0, 0]]])
+    chex = []
+    for c in co:
+        chex.append(colors.to_hex(c))
+    lgd_txt = '<span style="color: {col};">{txt}</span>'
+    grp = []
+    for ic, gn in enumerate(gnames):
+        grp.append(folium.FeatureGroup(name=lgd_txt.format(txt=gn, col=chex[ic])))
 
-coo = pd.read_csv('data/coord.csv')
-center = [31.41745, 35.06155]
-##
-map = folium.Map(location=center, zoom_start=7.5, tiles='cartodbpositron')
-# tiles = ['cartodbpositron', 'stamenterrain']
-# for tile in tiles:
-#     folium.TileLayer(tile).add_to(map)
-# folium.TileLayer('cartodbpositron').add_to(map)
-# folium.TileLayer('openstreetmap').add_to(map)
-# folium.TileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png',
-#                  attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors').add_to(map)
-map.get_root().html.add_child(folium.Element(title_html))
+    coo = pd.read_csv('data/coord.csv')
+    center = [31.41745, 35.06155]
+    ##
 
-for year in range(2019, current_year+1):
-    idx = (yyyy == year)
-    loc = np.asarray(prev['cities'][idx])
-    locu = np.unique(loc)
-    size = np.zeros(len(locu), int)
-    for iloc in range(len(locu)):
-        row_coo = coo['loc'] == locu[iloc]
-        if np.sum(row_coo) == 1:
-            size[iloc] = np.sum(loc == locu[iloc])
-            lat = float(coo['lat'][row_coo])
-            long = float(coo['long'][coo['loc'] == locu[iloc]])
-            tip = locu[iloc]+'('+str(year) + '):  ' + str(size[iloc])  # + str(mag[ii]) + depth  + '<br> '
-            folium.CircleMarker(location=[lat, long],
-                                tooltip=tip,
-                                radius=float(np.max([size[iloc]**0.5*2, 1])),
-                                fill=True,
-                                fill_color=chex[year-2019],
-                                color=chex[year-2019],
-                                opacity=0.5,
-                                fill_opacity=0.5
-                                ).add_to(grp[year-2019])
-        else:
-            print('cannot find coord for '+locu[iloc])
-pasts = [np.where(past_7d)[0], np.where(past_24h)[0]]
-for last in [0, 1]:
-    idx = pasts[last]
-    if len(idx) > 0:
+    map = folium.Map(location=center, zoom_start=7.5, tiles='cartodbpositron')
+    # tiles = ['cartodbpositron', 'stamenterrain']
+    # for tile in tiles:
+    #     folium.TileLayer(tile).add_to(map)
+    # folium.TileLayer('cartodbpositron').add_to(map)
+    # folium.TileLayer('openstreetmap').add_to(map)
+    # folium.TileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png',
+    #                  attr='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors').add_to(map)
+    map.get_root().html.add_child(folium.Element(title_html))
+
+    for year in range(2019, current_year+1):
+        idx = (yyyy == year)
         loc = np.asarray(prev['cities'][idx])
         locu = np.unique(loc)
         size = np.zeros(len(locu), int)
         for iloc in range(len(locu)):
-            size[iloc] = np.sum(loc == locu[iloc])
-            lat = float(coo['lat'][coo['loc'] == locu[iloc]])
-            long = float(coo['long'][coo['loc'] == locu[iloc]])
-            tip = locu[iloc]+':  ' + str(size[iloc])  # + str(mag[ii]) + depth  + '<br> '
-            folium.CircleMarker(location=[lat, long],
-                                tooltip=tip,
-                                radius=float(np.max([size[iloc]**0.5*2, 1])),
-                                fill=True,
-                                fill_color=chex[len(gnames)-2+last],
-                                color=chex[len(gnames)-2+last],
-                                opacity=0,
-                                fill_opacity=0.5
-                                ).add_to(grp[len(gnames)-2+last])
+            row_coo = coo['loc'] == locu[iloc]
+            if np.sum(row_coo) == 1:
+                size[iloc] = np.sum(loc == locu[iloc])
+                lat = float(coo['lat'][row_coo])
+                long = float(coo['long'][coo['loc'] == locu[iloc]])
+                tip = locu[iloc]+'('+str(year) + '):  ' + str(size[iloc])  # + str(mag[ii]) + depth  + '<br> '
+                folium.CircleMarker(location=[lat, long],
+                                    tooltip=tip,
+                                    radius=float(np.max([size[iloc]**0.5*2, 1])),
+                                    fill=True,
+                                    fill_color=chex[year-2019],
+                                    color=chex[year-2019],
+                                    opacity=0.5,
+                                    fill_opacity=0.5
+                                    ).add_to(grp[year-2019])
+            else:
+                print('cannot find coord for '+locu[iloc])
+    pasts = [np.where(past_7d)[0], np.where(past_24h)[0]]
+    for last in [0, 1]:
+        idx = pasts[last]
+        if len(idx) > 0:
+            loc = np.asarray(prev['cities'][idx])
+            locu = np.unique(loc)
+            size = np.zeros(len(locu), int)
+            for iloc in range(len(locu)):
+                size[iloc] = np.sum(loc == locu[iloc])
+                lat = float(coo['lat'][coo['loc'] == locu[iloc]])
+                long = float(coo['long'][coo['loc'] == locu[iloc]])
+                tip = locu[iloc]+':  ' + str(size[iloc])  # + str(mag[ii]) + depth  + '<br> '
+                folium.CircleMarker(location=[lat, long],
+                                    tooltip=tip,
+                                    radius=float(np.max([size[iloc]**0.5*2, 1])),
+                                    fill=True,
+                                    fill_color=chex[len(gnames)-2+last],
+                                    color=chex[len(gnames)-2+last],
+                                    opacity=0,
+                                    fill_opacity=0.5
+                                    ).add_to(grp[len(gnames)-2+last])
 
 
-for ig in range(len(gnames)):
-    grp[ig].add_to(map)
-folium.map.LayerControl('topleft', collapsed=False).add_to(map)
-html_name = "docs/alarms_by_year.html"
-map.save(html_name)
+    for ig in range(len(gnames)):
+        grp[ig].add_to(map)
+    folium.map.LayerControl('topleft', collapsed=False).add_to(map)
+    html_name = "docs/alarms_by_year.html"
+    map.save(html_name)
 # with open(html_name, 'r') as fid:
 #     html = fid.read()
 # osmde = 'https://tile.openstreetmap.de/{z}/{x}/{y}.png'  # 'openstreetmap.de'
