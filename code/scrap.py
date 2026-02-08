f86b22f6a39d989b592ca587e91e0b5cfab5675b
yuval <yuval@lenovo.2020>
2026-02-08 13:58:00 +0200

    scrap

--- code/scrap.py
+++ code/scrap.py
@@ -3,11 +3,4 @@ import numpy as np
 import os
 
 miss = pd.read_csv('~/Documents/oct7database - no coord.csv')
-miss.sort_values('שם משפחה', inplace=True)
-txt = ''
-for ii in range(len(miss)):
-    txt += f"{miss.iloc[ii]['שם משפחה']} {miss.iloc[ii]['שם פרטי']} {miss.iloc[ii]['שם נוסף']}, "
-print(txt)
-txt = txt.replace(' nan,', ',')
-with open('/home/innereye/Documents/missing.txt', 'w') as f:
-    f.write(txt)
+print(miss.head())
